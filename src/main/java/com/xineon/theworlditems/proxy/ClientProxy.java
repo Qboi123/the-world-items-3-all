047a9a2b4106103a16e2abc83e0a81b2b7a40481
quint <30625109+Qboi123@users.noreply.github.com>
2022-01-19 20:25:33 +0100

    0.0.1

---
+++ src/main/java/com/xineon/theworlditems/proxy/ClientProxy.java
@@ -0,0 +1,7 @@
+package com.xineon.theworlditems.proxy;
+
+import com.xineon.theworlditems.proxy.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+
+}
---
+++ src/main/java/com/xineon/theworlditems/proxy/CommonProxy.java
@@ -0,0 +1,6 @@
+package com.xineon.theworlditems.proxy;
+
+
+public class CommonProxy {
+
+}
